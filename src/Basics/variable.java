d2208f9750a0d5a39bbc4b9db69764b1f6e3d6a1
Abhishek <hopesaliveabhi2003@gmail.com>
2025-04-28 20:16:36 +0530

    javabasic

---
+++ src/Basics/variable.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class variable {
+    public static void main(String[] args) {
+        int x;//Declaration of variable
+        x = 6;//Initialisation of var.
+        System.out.println(x);//this 'x' prints variable and a var can only be printed iff it's initialised earlier.
+
+        System.out.println("x");// anything under double quotes is treated as a string or character and printed exactly the same.
+
+        //next method of declaration & initialisation
+
+        int y = 8;//Initialization & Declaration
+        System.out.println(x+y);
+    }
+}
