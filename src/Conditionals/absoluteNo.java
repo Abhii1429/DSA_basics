22ab36e182583bd776eabb360c797d5ce61cd63e
Abhishek <hopesaliveabhi2003@gmail.com>
2025-04-29 23:16:45 +0530

    conditionals

---
+++ src/Conditionals/absoluteNo.java
@@ -0,0 +1,11 @@
+package Conditionals;
+import java.util.Scanner;
+public class absoluteNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any integer : ");
+        int n = sc.nextInt();
+        int a = Math.abs(n);
+        System.out.println("Absolute No : " +a);
+    }
+}
---
+++ src/Conditionals/absoluteNumber.java
@@ -0,0 +1,16 @@
+package Conditionals;
+
+import java.util.Scanner;
+
+public class absoluteNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any Number : ");
+        int n = sc.nextInt();
+        if(n >= 0) {
+            System.out.println("Absolute value is " + n);
+        } else{
+                System.out.println("Absolute value is "+ -n);
+            }
+    }
+}
