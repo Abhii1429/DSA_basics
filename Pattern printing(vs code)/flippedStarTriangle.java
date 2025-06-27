4d2996c5be7180445ddfcd912e65958948e42d75
Abhishek Pandey <121479460+Abhii1429@users.noreply.github.com>
2025-06-27 14:56:59 +0000

    pp

---
+++ Pattern printing(vs code)/flippedStarTriangle.java
@@ -0,0 +1,18 @@
+package PatternPrinting;
+import java.util.Scanner;
+public class flippedStarTriangle {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter row m:");
+        int m = sc.nextInt();
+        System.out.print("Enter column n :");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n+1-i; j++){
+                System.out.print("*" +" ");
+            }
+            System.out.println();
+        }
+    }
+}
