46598018433d89f260fe905e1713c783e8c8363c
Abhishek Pandey <121479460+Abhii1429@users.noreply.github.com>
2025-06-27 14:54:23 +0000

    pp

---
+++ Pattern printing(vs code)/starSquare.java
@@ -0,0 +1,16 @@
+package PatternPrinting;
+import java.util.Scanner;
+public class starSquare {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the side of Square :");
+        int a = sc.nextInt();
+
+        for( int i = 1; i <= a; i++){
+            for(int j = 1; j <= a; j++){
+                System.out.print("* ");
+            }
+          System.out.println();  
+        }
+    }
+}
