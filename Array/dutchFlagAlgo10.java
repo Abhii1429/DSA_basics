c76161674562ec453a6a3eaf906fe98abbb57939
Abhishek <hopesaliveabhi2003@gmail.com>
2025-07-16 18:56:26 +0530

    Dutch Flag Algo(Sort Color-LC75)

---
+++ Array/dutchFlagAlgo10.java
@@ -0,0 +1,42 @@
+package Array;
+
+public class dutchFlagAlgo10 {
+    public static void main(String[] args){
+        int[] arr = {0,1,2,0,1,2,1,2,0,0};
+        int n = arr.length;
+
+        for(int ele : arr){
+            System.out.print(ele + " ");
+        }
+        System.out.println();
+
+        // Method-1:
+        int noOfZeroes = 0, noOfOnes = 0;
+         for(int i = 0; i < n; i++){
+            if(arr[i] == 0) noOfZeroes++;
+            if(arr[i] == 1) noOfOnes++;            
+        }
+        // my approach
+        //  for(int i = 0; i < noOfZeroes; i++){
+        //     arr[i] = 0;
+        // }
+        // for (int i = noOfZeroes; i < noOfOnes; i++) {
+        //     arr[i] = 1;
+        // }
+        // for (int i = noOfOnes; i < n; i++) {
+        //     arr[i] = 2;
+        // }
+
+        
+        for (int i = 0; i < n; i++) {
+            if(i < noOfZeroes) arr[i] = 0;
+            else if(i < noOfZeroes + noOfOnes) arr[i] = 1;
+            else arr[i] = 2;
+        }
+        for(int ele : arr){
+            System.out.print(ele +" ");
+        }
+        System.out.println();     
+    }
+}
+
