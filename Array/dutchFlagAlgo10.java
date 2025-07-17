6dba310ce905c2a86e82a6c6c39159b6fff259b5
Abhishek <hopesaliveabhi2003@gmail.com>
2025-07-17 17:35:15 +0530

    Dutch Flag Algo(Sort Color-LC75)

--- Array/dutchFlagAlgo10.java
+++ Array/dutchFlagAlgo10.java
@@ -10,33 +10,55 @@ public class dutchFlagAlgo10 {
         }
         System.out.println();
 
-        // Method-1:
-        int noOfZeroes = 0, noOfOnes = 0;
-         for(int i = 0; i < n; i++){
-            if(arr[i] == 0) noOfZeroes++;
-            if(arr[i] == 1) noOfOnes++;            
-        }
-        // my approach
-        //  for(int i = 0; i < noOfZeroes; i++){
-        //     arr[i] = 0;
+        // // Method-1:Two pass approach
+        // int noOfZeroes = 0, noOfOnes = 0;
+        //  for(int i = 0; i < n; i++){
+        //     if(arr[i] == 0) noOfZeroes++;
+        //     if(arr[i] == 1) noOfOnes++;            
         // }
-        // for (int i = noOfZeroes; i < noOfOnes; i++) {
-        //     arr[i] = 1;
-        // }
-        // for (int i = noOfOnes; i < n; i++) {
-        //     arr[i] = 2;
-        // }
+        // // my approach
+        // //  for(int i = 0; i < noOfZeroes; i++){
+        // //     arr[i] = 0;
+        // // }
+        // // for (int i = noOfZeroes; i < noOfOnes; i++) {
+        // //     arr[i] = 1;
+        // // }
+        // // for (int i = noOfOnes; i < n; i++) {
+        // //     arr[i] = 2;
+        // // }
 
+        // // sir's approach
+        // for (int i = 0; i < n; i++) {
+        //     if(i < noOfZeroes) arr[i] = 0;
+        //     else if(i < noOfZeroes + noOfOnes) arr[i] = 1;
+        //     else arr[i] = 2;
+        // }
+        // for(int ele : arr){
+        //     System.out.print(ele +" ");
+        // }
+        // System.out.println();  
         
+        // Method-3: Two pass approach(3-pointer approach)
+        int mid = 0, high = n-1, low = 0;
+        while(mid <= high){
+            if(arr[mid] == 0){
+                int temp = arr[mid];
+                arr[mid] = arr[low];
+                arr[low] = temp;
+                low++;
+                mid++;
+            }
+            else if(arr[mid] == 1) mid++;
+            else{
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
         for (int i = 0; i < n; i++) {
-            if(i < noOfZeroes) arr[i] = 0;
-            else if(i < noOfZeroes + noOfOnes) arr[i] = 1;
-            else arr[i] = 2;
+            System.out.print(arr[i] +" ");
         }
-        for(int ele : arr){
-            System.out.print(ele +" ");
-        }
-        System.out.println();     
     }
 }
-
+ 
