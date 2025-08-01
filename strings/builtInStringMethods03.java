fd4e0f8358a6a23414e5f02a571aaf7f598cc4f6
Abhishek <hopesaliveabhi2003@gmail.com>
2025-08-01 18:30:55 +0530

    string

---
+++ strings/builtInStringMethods03.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class builtInStringMethods03 {
+    public static void main(String[] args) {
+        String s = "Abhishek Pandey";
+        System.out.println(s.charAt(5));
+        System.out.println(s.charAt(12));
+
+        int n = s.length();
+        System.out.println(n);
+    }
+}
